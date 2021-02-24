05511f9bd3cac0b6a62ad9fcbeeb53365a8575ed
teodormardale <mardaleteodorandrei@gmail.com>
2021-02-24 09:38:36 +0100

    Actualizar

--- 1DAM/BD/dml_game_management_V2.sql
+++ 1DAM/BD/dml_game_management_V2.sql
@@ -4,13 +4,16 @@ USE game_management;
 -- a. Mediante un solo comando: Action, Adventure, Fighting, Misc, Platform, Puzzle, 
 -- b. Cada opción un comando en un insert: Racing, Role-Playing, Shooter, Simulation, Sports, Strategy
 
-INSERT INTO genres VALUES (NULL, 'Action'), (NULL, 'Adventure'), (NULL, 'Fighting'), (NULL, 'Misc'), (NULL, 'Platform'), (NULL, 'Puzzle');
-INSERT INTO genres VALUES (NULL, 'Racing');
-INSERT INTO genres VALUES (NULL, 'Role-Playing');
-INSERT INTO genres VALUES (NULL, 'Shooter');
-INSERT INTO genres VALUES (NULL, 'Simulation');
-INSERT INTO genres VALUES (NULL, 'Sports');
-INSERT INTO genres VALUES (NULL, 'Strategy');
+INSERT INTO genres (genres_name) VALUES ('Action'), ('Adventure'), ('Fighting'), ('Misc'), ('Platform'), ('Puzzle');
+
+SELECT * FROM genres;
+
+INSERT INTO genres (genres_name) VALUES ('Racing');
+INSERT INTO genres (genres_name) VALUES ('Role-Playing');
+INSERT INTO genres (genres_name) VALUES ('Shooter');
+INSERT INTO genres (genres_name) VALUES ('Simulation');
+INSERT INTO genres (genres_name) VALUES ('Sports');
+INSERT INTO genres (genres_name) VALUES ('Strategy');
 
 SELECT * FROM genres;
 
@@ -32,14 +35,14 @@ SELECT * FROM platforms;
 -- (1,'10TACLE Studios'),(2,'1C Company'),(3,'20th Century Fox Video Games')
 -- (4,'Activision'),(5,'Activision Blizzard'),(6,'Activision Value')
 
-INSERT INTO publishers VALUES (NULL, '10TACLE Studios'), (NULL, '1C Company'), (NULL, '20th Century Fox Video Games'), (NULL, 'Activision'), (NULL, 'Activision Blizzard'), (NULL, 'Activision Value');
+INSERT INTO publishers (publisher_names) VALUES ('10TACLE Studios'), ('1C Company'), ('20th Century Fox Video Games'), ('Activision'), ('Activision Blizzard'), ('Activision Value');
 
 SELECT * FROM publishers;
 
 -- 4. Crea los siguientes registros en la tabla regions mediante un solo comando insert
 -- (1,'North America'),(2,'Europe'),(3,'Japan'),(4,'Other')
 
-INSERT INTO regions VALUES (NULL, 'North America'), (NULL, 'Europe'), (NULL, 'Japan'), (NULL, 'Other');
+INSERT INTO regions (region_names) VALUES ('North America'), ('Europe'), ('Japan'), ('Other');
 
 SELECT * FROM regions;
 
@@ -48,31 +51,31 @@ SELECT * FROM regions;
 -- b. NOTA: los campos deben guardar por defecto la fecha actual
 
 ALTER TABLE games ADD (
-created_at DATETIME DEFAULT NULL,
-updated_at DATETIME DEFAULT NULL
+created_at DATETIME DEFAULT NOW(),
+updated_at DATETIME DEFAULT NOW()
 );
 
-INSERT INTO games (created_at, updated_at)
+INSERT INTO games (genres_id, games_name)
 VALUES (CURRENT_TIME(), CURRENT_TIME());
 
 SELECT * FROM games;
 
 ALTER TABLE game_publishers ADD (
-created_at DATETIME DEFAULT NULL,
-updated_at DATETIME DEFAULT NULL
+created_at DATETIME DEFAULT NOW(),
+updated_at DATETIME DEFAULT NOW()
 );
 
-INSERT INTO game_publishers (created_at, updated_at)
+INSERT INTO game_publishers (games_id, publishers_id)
 VALUES (CURRENT_TIME(), CURRENT_TIME());
 
 SELECT * FROM game_publishers;
 
 ALTER TABLE game_platforms ADD (
-created_at DATETIME DEFAULT NULL,
-updated_at DATETIME DEFAULT NULL
+created_at DATETIME DEFAULT NOW(),
+updated_at DATETIME DEFAULT NOW()
 );
 
-INSERT INTO game_platforms (created_at, updated_at)
-VALUES (CURRENT_TIME(), CURRENT_TIME());
+INSERT INTO game_platforms (game_publishers_id, platforms_id, release_years)
+VALUES (CURRENT_TIME(), CURRENT_TIME(), CURRENT_TIME());
 
 SELECT * FROM game_platforms;
